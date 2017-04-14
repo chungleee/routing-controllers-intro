0cb676ae7d68d1d6cc614037cc03ad2a9d1252ab
Leon <leon.karho.chung@gmail.com>
2017-04-14 15:38:48 -0400

    define dynamic route for Kittens page, setup kittens method in controller, set value for kitten_url var, DRY-ed code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  before_action :set_kitten_url, only: [:kitten, :kittens]
+
   def welcome
     @header = "BRUHHHHHHHUHUHUHUHUHUHU"
   end
@@ -12,6 +14,14 @@ class PagesController < ApplicationController
   end
 
   def kitten
+    # set_kitten_url
+  end
+
+  def kittens
+    # set_kitten_url
+  end
+
+  def set_kitten_url
     requested_size = params[:size]
     @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
   end
